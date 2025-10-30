eb932519e4f84af27d53c925d9befb3b44acab98
wangshuxian <airmusic@msn.com>
2025-10-31 01:15:37 +0800

    04_4 请求体

--- app/main.py
+++ app/main.py
@@ -6,10 +6,6 @@ shipments = {
     1: {"content": "wooden table", "status": "in transit", "weight": 1.2},
     2: {"content": "glassware", "status": "placed", "weight": 0.6},
     3: {"content": "office chair", "status": "dispatched", "weight": 3.5},
-    4: {"content": "books", "status": "delivered", "weight": 2.1},
-    5: {"content": "gaming laptop", "status": "packed", "weight": 1.8},
-    6: {"content": "microwave oven", "status": "in transit", "weight": 6.0},
-    7: {"content": "desk lamp", "status": "processing", "weight": 0.9},
 }
 
 app = FastAPI()
@@ -32,28 +28,27 @@ def get_shipment(id: int | None = None) -> dict[str, Any]:
         )
     return shipments[id]
 
+
 @app.post("/shipment")
-def submit_shipment(content: str, weight: float) -> dict[str, int]:
-    # 如果重量超过限制，抛出异常
+def submit_shipment(data: dict[str, Any]) -> dict[str, Any]:
+    # 获取内容与重量
+    content = data["content"]
+    weight = data["weight"]
+
+    # 重量校验
     if weight > 25:
         raise HTTPException(
             status_code=status.HTTP_406_NOT_ACCEPTABLE,
             detail="Maximum weight limit is 25 kg",
         )
 
-    # 计算新的 ID
-    last_id = max(shipments.keys())
-    new_id = last_id + 1
-
-    # 构建新发货数据
-    new_shipment = {
+    # 创建新 ID
+    new_id = max(shipments.keys()) + 1
+    shipments[new_id] = {
         "content": content,
         "weight": weight,
         "status": "placed",
     }
 
-    # 添加到数据字典中
-    shipments[new_id] = new_shipment
-
-    # 返回新创建的 ID
+    # 返回新创建的发货 ID
     return {"id": new_id}
